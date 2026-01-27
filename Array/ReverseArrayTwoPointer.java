d112247ddd00a9493cb140183121ae7b1bad042d
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-01-27 23:21:32 +0530

    a

--- Array/ReverseArrayTwoPointer.java
+++ Array/ReverseArrayTwoPointer.java
@@ -1,5 +1,28 @@
 package Array;
 
 public class ReverseArrayTwoPointer {
+    public static void ReverseArrayTwoPointer(int arr[]){
+            int start=0;
+            int end=arr.length-1;
+
+            while(start<end){
+                int temp=arr[start];
+                arr[start]=arr[end];
+                arr[end]=temp;
+                start++;
+                end--;
+            }
+        
+        for(int x:arr){
+            System.out.print(x+" ");
+        }
+
+    }
+     
+
+    public static void main(String[] args) {
+     int arr[]={10,20,30,40,50};
+     ReverseArrayTwoPointer(arr);
+    }
     
 }
