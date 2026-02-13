ca038f9102344c80217bcc21cd04b0e80ce6e905
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-02-14 01:06:24 +0530

    rotate array

---
+++ IProgrammer/rotate.java
@@ -0,0 +1,20 @@
+package IProgrammer;
+
+public class rotate {
+public static void rotateArray(int arr[]){
+    int last=arr[arr.length-1];
+    for(int i=arr.length-1;i>0;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=last;
+    for(int x: arr){
+        System.out.print(x+" ");
+    }
+}
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        rotateArray(arr);
+    }
+    
+}
