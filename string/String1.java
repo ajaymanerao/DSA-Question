a6a7164e00e32f72232b02e6590fd89f05be25fe
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-17 15:24:05 +0530

    String

---
+++ string/String1.java
@@ -0,0 +1,18 @@
+package string;
+
+public class String1 {
+
+    public static void main(String[] args) {
+
+        String s1="KareenaKapoor";
+        final String s2="Kareena";
+        String s3= s2+"Kapoor";
+        System.out.println(s1==s3);
+        
+
+
+
+
+    }
+    
+}
