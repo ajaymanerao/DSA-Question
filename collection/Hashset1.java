5026dc6f069565eff309de5446b01790595f1d7a
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-17 14:19:16 +0530

    HashSet

---
+++ collection/Hashset1.java
@@ -0,0 +1,23 @@
+package collection;
+
+import java.util.HashSet;
+
+public class Hashset1 {
+    public static void main(String[] args) {
+        
+        HashSet<Integer>box=new HashSet<Integer>();
+        box.add(11);
+        box.add(111);
+        box.add(12);
+		box.add(21);
+		box.add(342);
+		box.add(12);
+		box.add(111);
+		box.add(11);
+		box.add(null);
+
+        System.out.println(box);
+        System.out.println(box.contains(11));
+    }
+    
+}
