c5e90cd548b05b8e39539a7bfa126dd0bfa24100
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-20 22:48:28 +0530

    Syso

--- App.java
+++ App.java
@@ -4,6 +4,10 @@ public void main(String[] args) {
     int a=10;
     int b=20;
     System.out.println(a+b);
+        System.out.println("10"+20+30);
+        System.out.println(10+"20"+30);
+        System.out.println(10+20+"30");
+        System.out.println("10"+2*6);
 }
     
 }
