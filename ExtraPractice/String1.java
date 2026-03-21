68fcb332e893ad64f951333b7611b64cd26affc3
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-21 17:42:14 +0530

    String.equals(StrBuffer)

--- ExtraPractice/String1.java
+++ ExtraPractice/String1.java
@@ -2,19 +2,43 @@ package ExtraPractice;
 
 public class String1 {
     public static void main(String[] args) {
-        String s1="Ajay";
-        String s2="Anerao";
-        System.out.println(s1.compareTo(s2));
+        // String s1="Ajay";
+        // String s2="Anerao";
+        // System.out.println(s1.compareTo(s2));
 
 
-        String s3="Ajay";
-        String s4="Ajay";
-        System.out.println(s3.compareTo(s4));
+        // String s3="Ajay";
+        // String s4="Ajay";
+        // System.out.println(s3.compareTo(s4));
 
 
-        String s5="Zjay";
-        String s6="Neha";
-        System.out.println(s5.compareTo(s6));
+        // String s5="Zjay";
+        // String s6="Neha";
+        // System.out.println(s5.compareTo(s6));
+
+        // System.out.println("A".compareTo("a"));
+
+        // String str="AjayAnerao";
+        // String s=str.replace('A', 'B');
+        // System.out.println(s);
+        
+        // String str1="AjayAnerao";
+        // String a=str1.replace('A', 'z');
+        // System.out.println(a);
+
+        // String str2="AjayAnerao";
+        // String b=str2.replaceAll("A", "B");
+        // System.out.println(b);
+
+
+        String str2=new String("Ajay");
+        StringBuffer sb=new StringBuffer("Ajay");
+        System.out.println(str2.equals(sb));
+        
+
+
+
+        
 
 
 
