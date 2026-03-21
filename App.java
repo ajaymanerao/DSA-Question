e91caf0890bf594be566ccf0d949823b0713876c
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-21 15:58:44 +0530

    String

--- App.java
+++ App.java
@@ -1,13 +1,29 @@
 public class App {
 
 public void main(String[] args) {
-    int a=10;
-    int b=20;
-    System.out.println(a+b);
-        System.out.println("10"+20+30);
-        System.out.println(10+"20"+30);
-        System.out.println(10+20+"30");
-        System.out.println("10"+2*6);
+    // int a=10;
+    // int b=20;
+    // System.out.println(a+b);
+    // System.out.println(a-b);
+        // System.out.println("10"+20+30);
+        // System.out.println(10+"20"+30);
+        // System.out.println(10+20+"30");
+        // System.out.println("10"+2*6);
+
+        int a=1200;
+        int rev=0;
+        while(a!=0){
+            int d=a%10;
+            rev=rev*10+d;
+            a/=10;
+
+        }
+        System.out.println(rev);
+
+        //print("10"+"20"*3); 10202020
+
+
+
 }
     
 }
---
+++ ExtraPractice/String1.java
@@ -0,0 +1,10 @@
+package ExtraPractice;
+
+public class String1 {
+    public static void main(String[] args) {
+        String s1="Ajay";
+        String s2="Anerao";
+        System.out.println(s1.compareTo(s2));
+    }
+    
+}
