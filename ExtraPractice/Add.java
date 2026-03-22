5c9aa831733d07e44b320eb18c68cd78d8680646
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-22 22:43:13 +0530

    A

--- ExtraPractice/Add.java
+++ ExtraPractice/Add.java
@@ -5,13 +5,13 @@ public class Add {
     void main(){
         //System.out.println(1+1);
 
-        String str1="Ajay";
-        String str2="Ajay";
+        String str1="Animal";
+        String str2="Animal";
         //System.out.println(str1.equals(str2));
 
 
-        StringBuffer str3=new StringBuffer("Ajay");
-        StringBuffer str4=new StringBuffer("Ajay");
+        StringBuffer str3=new StringBuffer("Animal");
+        StringBuffer str4=new StringBuffer("Animal");
         // System.out.println(str3.equals(str4));
         System.out.println(str3.compareTo(str4));
         System.out.println(str1.contentEquals(str4));
