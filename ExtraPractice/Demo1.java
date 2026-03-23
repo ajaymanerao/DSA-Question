1035fd80b6c58b24854035a556b076b7ef734c8d
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-23 09:37:57 +0530

    StringBuffer

---
+++ ExtraPractice/Demo1.java
@@ -0,0 +1,16 @@
+package ExtraPractice;
+
+ 
+
+public class Demo1 {
+     void main() {
+         
+
+        StringBuffer str1=new StringBuffer("Ajay");
+        StringBuffer str2=new StringBuffer("Ajay");
+        System.out.println(str1.toString().equals(str2.toString()));
+
+        
+    }
+    
+}
