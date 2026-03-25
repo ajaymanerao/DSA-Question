08e0372324c36fefc7a177d7e5d73a340960d584
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-25 10:21:35 +0530

    var

--- ExtraPractice/Add.java
+++ ExtraPractice/Add.java
@@ -18,6 +18,15 @@ public class Add {
         System.out.println(str3.toString().equals(str4.toString())); // content check instring buffer
         System.out.println(str1.equals(str4));
 
+        //var it say to compiler you decide the datatype based on the value i assign
+        //var is not the datatype it is the keyword of java
+        var a=5;
+        var b=6;
+        System.out.println(a+b);
+        var c=44.44;
+        var e=55.55;
+        System.out.println(c+e);
+
 
     }
     
