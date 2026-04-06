5540aeec8cb29dc6ed19f28828b7e84e1beb8879
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-04-06 14:03:27 +0530

    A

--- FunctionalPro/Program7.java
+++ FunctionalPro/Program7.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program7 {
@@ -44,6 +46,15 @@ public class Program7 {
                .map(String::length)
                .forEach(System.out::println);
 
+
+        System.out.println("Collection result");
+
+        List<String>finalResult=friends.stream()
+                                        .map(String::toUpperCase)
+                                        .collect(Collectors.toList());
+
+
+       System.out.println(finalResult);
 
 
 
