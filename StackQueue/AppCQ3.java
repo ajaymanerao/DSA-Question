8e535f3301b9274ced8e640f4399b20b6c6fc555
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-04-14 14:13:39 +0530

    Circular Queue

---
+++ StackQueue/AppCQ3.java
@@ -0,0 +1,27 @@
+package StackQueue;
+
+public class AppCQ3 {
+    public static void main(String[] args) {
+        CircularQueue circularQueue=new CircularQueue(5);
+        System.out.println("is Empty  " + circularQueue.isEmpty());
+		circularQueue.enqueue(11);
+		circularQueue.enqueue(22);
+		circularQueue.enqueue(33);
+		circularQueue.enqueue(44);
+		circularQueue.enqueue(55);
+		circularQueue.enqueue(66);
+
+        System.out.println(circularQueue);
+
+        System.out.println(circularQueue.peek());
+
+        System.out.println(" ==> "+circularQueue.dequeue());
+
+        while(!circularQueue.isEmpty()){
+            System.out.println(" ==> "+ circularQueue.dequeue());
+        }
+
+        circularQueue.enqueue(66);
+    }
+    
+}
