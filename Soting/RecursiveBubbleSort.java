6f21f621722cdf806db03a20bdc9a7d4d3ab75ca
ajaymanerao <ajayanerao76@gmail.com>
2026-05-19 13:28:56 +0530

    recusive method of bubble Sort

---
+++ Soting/RecursiveBubbleSort.java
@@ -0,0 +1,42 @@
+package Soting;
+
+import java.util.Scanner;
+
+public class RecursiveBubbleSort {
+    public static void recursiveBubbleSort(int arr[], int n){
+        if(n==1)
+            return;
+
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+            }
+        }
+
+        recursiveBubbleSort(arr, n-1);
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int [n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        recursiveBubbleSort(arr, n);
+
+
+        System.out.println("Sorted Array");
+        System.out.println("------------------------");
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+
+        
+    }
+    
+}
